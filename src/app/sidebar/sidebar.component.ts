ddec19a32477ab5a90aea5fda93a3b97ce35392d
ElGfm <el.gfm97@gmail.com>
2021-01-23 22:33:48 +0100

    Add docs

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor() {
+  }
+
+
+
+}
