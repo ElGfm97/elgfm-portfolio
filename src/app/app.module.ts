d87217c2e3afba5925e355b65801d506c3347126
ElGfm <el.gfm97@gmail.com>
2021-03-19 21:55:51 +0100

    Added background for home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
@@ -29,7 +28,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     ProjectsComponent,
     ContactsComponent,
     HomeComponent,
-    SidebarComponent
 
   ],
   imports: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  bgImagesArray = [  // array of background image names
+    'bg1.png',
+    'bg2.jpg',
+    'bg3.jpg',
+    'bg4.jpg',
+    'bg5.jpg',
+    'bg6.jpg',
+    'bg7.jpg',
+    'bg8.jpg',
+    'bg9.jpg'
+  ];
+  randomImage = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.randomImage = this.bgImagesArray[Math.floor(Math.random() * 9)]; // on every initialization of component bg image is random
   }
 
 
